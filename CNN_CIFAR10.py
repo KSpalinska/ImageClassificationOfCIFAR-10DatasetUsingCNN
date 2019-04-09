934b04a4b36dffdd2633223f739bc1aa115052de
Agata Paldyna <paldyna.agata@gmail.com>
2019-04-09 16:53:58 +0200

    Some cleaning in code

--- CNN_CIFAR10.py
+++ CNN_CIFAR10.py
@@ -15,6 +15,20 @@ from keras.utils import np_utils
 import time
 
 
+## Funkcje pomocnicze
+def accuracy(test_x, test_y, model):
+    """ 
+    Funkcja licząca precyzję (accuracy) 
+    dla danego zbioru danych i danego modelu 
+    """
+    result = model.predict(test_x)
+    predicted_class = np.argmax(result, axis = 1)
+    true_class = np.argmax(test_y, axis = 1)
+    num_correct = np.sum(predicted_class == true_class)
+    accuracy = float(num_correct)/result.shape[0]
+    return (accuracy * 100)
+
+
 ## Ładowanie zbiorów danych
 from keras.datasets import cifar10
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -23,47 +37,48 @@ n_train, img_row, img_col, img_ch = x_train.shape
 n_test = x_test.shape[0]
 categories = np.unique(y_train)
 n_categories = len(categories)
-number_of_layers=1
-batch_size=64
-epochs=1
 
 
-
-
-
-
-## Data pre-processing
+## Pre-processing danych
 x_train = x_train.astype('float32')/255
 x_test = x_test.astype('float32')/255
 
-#one-hot-encoding
-# convert class labels to binary class labels
+# one-hot-encoding - konwersja etykiet kategorii na binarne etykiety kategorii
 y_train = np_utils.to_categorical(y_train, n_categories)
 y_test = np_utils.to_categorical(y_test, n_categories)
 
-## Convolutional Neural Network for CIFAR-10 dataset
-# Define the model
+
+## Parametry sieci
+number_of_layers = 1
+batch_size = 64
+epochs = 1
+
+
+## CNN dla zbioru CIFAR-10
+
+# Zdefiniwanie modelu
 model = Sequential()
-#Add layers
+
+# Dodawanie warstw
 for i in range(number_of_layers):
-    if i ==0 :
-        #layer0
-        model.add(Convolution2D(32, (3, 3), padding='same',input_shape=x_train.shape[1:]))
+    if i == 0:
+        # layer0
+        model.add(Convolution2D(32, (3, 3), padding = 'same', input_shape = x_train.shape[1:]))
         model.add(Activation('relu'))
         model.add(Convolution2D(32, (3, 3)))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
         model.add(Dropout(0.25))
 
-    #next layers
-    model.add(Convolution2D(64, (3, 3), padding='same'))
+    # next layers
+    model.add(Convolution2D(64, (3, 3), padding = 'same'))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, (3, 3)))
     model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size = (2, 2)))
     model.add(Dropout(0.25))
 
-#flatten layers
+# flatten layers
 model.add(Flatten())
 model.add(Dense(512))
 model.add(Activation('relu'))
@@ -72,31 +87,29 @@ model.add(Dense(n_categories))
 model.add(Activation('softmax'))
 
 
-# Compile the model
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+## Konfiguracja procesu uczenia
+model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
+
+## Trenowanie modelu
 start = time.time()
-model_info = model.fit(x_train, y_train, batch_size=batch_size, \
-    epochs=epochs, validation_data=(x_test, y_test))
+model_info = model.fit(x_train, y_train, batch_size=batch_size, epochs = epochs, validation_data = (x_test, y_test))
 end = time.time()
 
-def accuracy(test_x, test_y, model):
-    result = model.predict(test_x)
-    predicted_class = np.argmax(result, axis=1)
-    true_class = np.argmax(test_y, axis=1)
-    num_correct = np.sum(predicted_class == true_class)
-    accuracy = float(num_correct)/result.shape[0]
-    return (accuracy * 100)
-
 print("Model took %0.2f seconds to train" % (end - start))
-# compute test accuracy
+
+
+## Precyzja na zbiorze testowym
 print("Accuracy on test data is: %0.2f" % accuracy(x_test, y_test, model))
-f = open("batch_size"+str(batch_size)+"_epochow_"+str(epochs),"w")
+
+
+## Zapis wyników do pliku
+f = open("batch_size_" + str(batch_size) + ";epochs_" + str(epochs), "w")
 
 for i in range(len(model_info.history['acc'])):
     f.write("acc: " + str(model_info.history['acc'][i]))
     f.write("\nval_acc: " + str(model_info.history['val_acc'][i]))
 
 f.close()
-#model_info.history[]
 
+#model_info.history[]
